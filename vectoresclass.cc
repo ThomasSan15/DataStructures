15a1defeece4dbf25a09e4d4e5d41c8ed2b55e04
Thomas Santiago <thomas.beltran@utp.edu.co>
2025-03-08 17:54:58 -0500

    merge

--- vectoresclass.cc
+++ vectoresclass.cc
@@ -59,7 +59,7 @@ class Vector{
 
     private:
     void resize(){
-        cout << "resize" << endl;
+       // cout << "resize" << endl;
         unsigned int capacity2 = capacity_ * 1.5;
        T* storage2 = new T[capacity2];
        for (unsigned int i = 0; i < size_; i++){
@@ -179,22 +179,76 @@ Vector<T> removeDuplicates(const Vector<T>& vector){
       return result;
     }
 
+    
+   template <typename T>
+   void Mergesort( Vector<T>& organizar){
+
+    if(organizar.size() == 1){
+        return;
+    }
+        Vector<T> leftVector;
+        Vector<T> rightVector;
+        
+     for(int i = 0; i < organizar.size(); i++){
+        if(i < (organizar.size() / 2)){
+         leftVector.push_back(organizar.at(i));
+        }else{
+            rightVector.push_back(organizar.at(i));
+        }
+     }
+     Mergesort(leftVector);
+     Mergesort(rightVector);
+
+     Vector<int> Organizado;
+     for(int i = 0, r = 0, l = 0; i < organizar.size(); i++){
+        if(leftVector.size() == l){
+            Organizado.push_back(rightVector.at(r));
+            r++;
+        } else if(rightVector.size() == r ){
+            Organizado.push_back(leftVector.at(l));
+            l++;
+        }else if(leftVector.at(l) < rightVector.at(r)){
+            Organizado.push_back(leftVector.at(l));
+            l++;
+        }
+        else{
+            Organizado.push_back(rightVector.at(r));
+            r++;
+        }
+       
+    }
+    organizar = Organizado;
+}
+
     template <typename T>
     Vector<T> mergeSortedVectors(Vector<T> vector1, Vector<T> vector2){
-      if(vector1.size() == 0){
+      if(!vector1.size()){
         return vector2;
       }
-      if(vector2.size() == 0){
+      if(!vector2.size()){
         return vector1;
       }
-      if(vector1.size() == 0 && vector2.size() == 0){
+      if(!vector1.size() && !vector2.size()){
         return Vector<T>();
       }
+
+      Vector<T> result;
+
+      for(unsigned int i = 0; i < vector1.size(); i++){
+        result.push_back(vector1[i]);
+      }
+      for(unsigned int i = 0; i < vector2.size(); i++){
+        result.push_back(vector2[i]);
+      }
+    
+        Mergesort(result);
+
+    return result;
+    }
       
 
-    }
-
  int main(){
+    /*
     Vector<int> numbers = {1, 2, 2, 3, 4, 4, 5};
     Vector<int> uniqueNumbers = removeDuplicates(numbers);
     
@@ -211,6 +265,27 @@ Vector<T> removeDuplicates(const Vector<T>& vector){
     Vector<int> numbers4 = {1};
     Vector<int> uniqueNumbers4 = removeDuplicates(numbers4);
     uniqueNumbers4.print(); // Expected: {1}
+  */
+ Vector<int> vector1 = {1, 1, 2, 4, 20, 9, 3, 5};
+Vector<int> vector2 = {2, 4, 6, 10, 2};
+Vector<int> mergedVector = mergeSortedVectors(vector1, vector2);
+
+mergedVector.print(); // Expected: {1, 2, 3, 4, 5, 6}
+
+Vector<int> vector3 = {1, 2, 3};
+Vector<int> vector4 = {};
+Vector<int> mergedVector2 = mergeSortedVectors(vector3, vector4);
+mergedVector2.print(); // Expected: {1, 2, 3}
+
+Vector<int> vector5 = {};
+Vector<int> vector6 = {4,5,6};
+Vector<int> mergedVector3 = mergeSortedVectors(vector5, vector6);
+mergedVector3.print(); // Expected: {4, 5, 6}
+
+Vector<int> vector7 = {1,1,1,1};
+Vector<int> vector8 = {1,1,1,1};
+Vector<int> mergedVector4 = mergeSortedVectors(vector7, vector8);
+mergedVector4.print(); // Expected: {1,1,1,1,1,1,1,1}
 
     return 0;
  }
