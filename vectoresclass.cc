17f07982466e7d2d886c506d8f8d583b3739f448
Thomas Santiago <thomas.beltran@utp.edu.co>
2025-03-08 14:47:00 -0500

    remove duplicates

--- vectoresclass.cc
+++ vectoresclass.cc
@@ -4,6 +4,7 @@
 using namespace std;
 
 template <typename T>
+
 class Vector{
     private:
     T* storage;
@@ -25,8 +26,17 @@ class Vector{
         }
         size_ = capacity_;
     }
-
-    unsigned int size(){
+    Vector(initializer_list<T> list){
+        capacity_ = list.size();  // La capacidad es el número de elementos en la lista
+        storage = new T[capacity_];
+        size_ = list.size();
+        unsigned int i = 0;
+        for (const T& elem : list) {
+            storage[i++] = elem;  // Copiamos cada elemento al array
+        }
+    }
+    
+    unsigned int size() const{
         return size_;
     }
 
@@ -34,7 +44,11 @@ class Vector{
         assert(position >= 0 && position < size_);
         return storage[position];
     }
-
+    
+    T& at(unsigned int position){
+        assert(position >= 0 && position < size_); // Asegura que la posición sea válida
+        return storage[position];
+    }
     const T& operator[](unsigned int index) const{
         return storage[index];
     }
@@ -61,6 +75,7 @@ class Vector{
         size_++;
     }
     void pop_back(){
+        assert(size_ > 0);
         size_--;
     }
 
@@ -76,21 +91,24 @@ class Vector{
     }
     delete[] storage;
        storage = storage2;
-       capacity_++;
+       size_++;
 }
   
 void pop_front(){
+    assert(size_ > 0);
     T* storage2 = new T[capacity_];
-      for(int i = 0; i < size_ ; i++){
+      for(unsigned int i = 0; i < size_ ; i++){
       storage2[i] = storage[i + 1]; 
   }
   delete[] storage;
      storage = storage2;
-    
+     size_--;
 }
 
+bool empty(){ return size_ == 0; }
+
  unsigned int waste(){
-    return capacity_ - size_;
+  return capacity_ - size_;
  }
 
  void shrink_to_fit(){;
@@ -101,10 +119,37 @@ void pop_front(){
     delete[] storage;
     capacity_ = size_;
     storage = storage2;
-
  }
 
- 
+ /*void insert(unsigned int index, const T& elem){  //insert que cambia un valor del vector en X posición
+    assert(index >= 0 && index < size_);
+    storage[position] = elem;  
+ }*/
+
+  void insert(unsigned int index, const T& elem){ //Insert que me agrega un nuevo valor
+    assert(index >= 0 && index < size_);
+    if (size_  == capacity_){
+        resize();
+    }
+     for(unsigned int i = size_; i > index; i--){
+       storage[i] = storage[i - 1];
+     }
+     storage[index] = elem;    
+     size_++;
+ }
+  void erase(unsigned int index){
+    assert(index >= 0 && index < size_);
+    T* storage2 = new T[capacity_];
+    for(unsigned int i = 0; i < size_; i++){
+        if(index != i){
+         storage2[i] = storage[i];
+        }
+    }
+    delete[] storage;
+    storage = storage2;
+    size_--;
+  }
+
     void print(){
         for(unsigned int i = 0; i < size_; i++){
             cout << " " << storage[i];
@@ -114,33 +159,42 @@ void pop_front(){
 
 };
 
-class  complex{
-    private:
-    double real;
-    double img;
-    public:
-    complex(){
-        real = 0.0;
-        img = 0.0;
-        }
-        friend ostream& operator<<(ostream& os, const complex& c);
-};
-
- ostream& operator<<(ostream& os, const complex& c){
-    os << "(" << c.real << "," << c.img << "i)";
-    return os;
+template <typename T>
+Vector<T> removeDuplicates(const Vector<T>& vector){
+    Vector<int> result;
+     for(unsigned int i = 0 , l = 0; i < vector.size(); i++){
+         bool bandera = true;
+         for(unsigned int j = 0; j <  result.size(); j++){
+             if(vector[i] == result[j]){
+            bandera = false;
+         }
+     } 
+     if(bandera){
+         result.push_back(vector[i]);
+     }
  }
+      return result;
+    }
+
+    template <typename T>
+    Vector<T> mergeSortedVectors(Vector<T> vector1, Vector<T> vector2){
+      if(vector1.size() == 0){
+        return vector2;
+      }
+      if(vector2.size() == 0){
+        return vector1;
+      }
+      if(vector1.size() == 0 && vector2.size() == 0){
+        return Vector<T>();
+      }
+      
+
+    }
 
  int main(){
-    Vector<int> x(10,0);
-    x.print();
-    x.push_front(5);
-    x.push_front(25);
-    x.push_front(55);
-    x.print();
-    cout << x.waste() << endl;
-    x.shrink_to_fit();
-    cout << x.waste() << endl;
-    
+
+    Vector<int> numbers = {1,1, 2, 2, 3, 4, 4,5,3,5,4, 5};
+    Vector<int> uniqueNumbers = removeDuplicates(numbers);
+    uniqueNumbers.print();
     return 0;
  }
